0ce371f16b397133382f1f1f4f2ab3654f425f38
sarasabet <sara.ilbeigi@gmail.com>
2021-12-07 14:24:17 -0500

    day 11

---
+++ 11.js
@@ -0,0 +1,26 @@
+
+const bookFreePlatform = (platformList, missionDate) => {
+  for ( obj  of platformList){ 
+    if( !obj.bookDate){
+      obj.bookDate = missionDate;
+      return platformList
+    }  
+  }
+
+}
+const missionDate = "2021-12-12"
+const platformList = [
+  {
+    name:"Platform A",
+    bookDate:"2021-12-11"
+  },
+  {
+    name:"Platform B",
+    bookDate:undefined
+  },
+  {
+    name:"Platform C",
+    bookDate:undefined
+  },
+] 
+console.log(bookFreePlatform (platformList, missionDate))
